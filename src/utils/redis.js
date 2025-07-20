3e4dd6e8322a46ebb0abc3edaed009d4e3f8f9a3
Kollipara Pavan Kumar <pavankumarkollipara2@gmail.com>
2025-07-21 02:24:48 +0530

    modified all codes

--- src/utils/redis.js
+++ src/utils/redis.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
 const { Redis } = require('ioredis');
 
-const redis = new Redis(process.env.REDIS_URL);
+const redis = new Redis(process.env.REDIS_URL, {
+  maxRetriesPerRequest: null, // 🔥 Required for BullMQ
+});
+
 module.exports = redis;
